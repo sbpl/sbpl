4ea525eda740e57a688762a0586f3550d6c63f14
maximl <maximl>
2010-03-10 20:52:01 +0000

    adding comments

--- src/utils/2Dgridsearch.h
+++ src/utils/2Dgridsearch.h
@@ -41,7 +41,7 @@ enum SBPL_2DGRIDSEARCH_OPENTYPE {SBPL_2DGRIDSEARCH_OPENTYPE_HEAP, SBPL_2DGRIDSEA
 //#define SBPL_2DGRIDSEARCH_HEUR2D(x,y)  ((int)(1000*cellSize_m_*sqrt((double)((x-goalX_)*(x-goalX_)+(y-goalY_)*(y-goalY_)))))
 #define SBPL_2DGRIDSEARCH_HEUR2D(x,y)  ((int)(1000*cellSize_m_*__max(abs(x-goalX_),abs(y-goalY_))))
 
-//search state
+//search state corresponding to each 2D cell
 class SBPL_2DGridSearchState : public AbstractSearchState
 {
 public:
@@ -51,6 +51,7 @@ public:
 
 	//search relevant data
 	int g;
+	//iteration at which the state was accessed (generated) last
 	int iterationaccessed;
 
 public:
@@ -64,6 +65,7 @@ public:
 class CIntHeap;
 class CSlidingBucket;
 
+//2D search itself
 class SBPL2DGridSearch
 {
  public:
@@ -71,12 +73,23 @@ class SBPL2DGridSearch
     SBPL2DGridSearch(int width_x, int height_y, float cellsize_m);
     ~SBPL2DGridSearch(){destroy();}
 
+	//the priority data structure used - it affects efficiency (can be either heap or sliding buckets, see definition of SBPL_2DGRIDSEARCH_OPENTYPE)
 	bool setOPENdatastructure(SBPL_2DGRIDSEARCH_OPENTYPE OPENtype);
 
+	//destroys the search and clears all memory
     void destroy();	
+
+	//performs search itself. All costs are given as cost(cell1, cell2) = Euclidean distance*1000*(max(cell1,cell2)+1) for adjacent cells. 
+	//It is infinite if max(cell1,cell2) >= obsthresh 
+	//For cells that are not adjacent (which may happen if 16 connected grid is ON), then max is taken over all the cells center line goes through
+	//Search is done from start to goal. termination_condition specifies when to stop the search. In particular, one may specify to run it until OPEN is
+	//empty. In this case, the values of all states will be computed.
 	bool search(unsigned char** Grid2D, unsigned char obsthresh, int startx_c, int starty_c, int goalx_c, int goaly_c, SBPL_2DGRIDSEARCH_TERM_CONDITION termination_condition);
 	
+	//print all the values
     void printvalues();
+
+	//returns the computed distance from the start to <x,y>. If not computed, then returns lower bound on it.
 	inline int getlowerboundoncostfromstart_inmm(int x, int y)
 	{
 		if(term_condition_usedlast == SBPL_2DGRIDSEARCH_TERM_CONDITION_OPTPATHFOUND)
@@ -97,6 +110,7 @@ class SBPL2DGridSearch
 		}
 	};
 	
+	//returns largest optimal g-value computed by search - a lower bound on the state values of unexpanded states
 	int getlargestcomputedoptimalf_inmm() {return largestcomputedoptf_;};
 
 
