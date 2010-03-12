46467997e5ff656fcf584385ba6931b518474b35
maximl <maximl>
2010-03-12 04:30:05 +0000

    comments

--- src/discrete_space_information/navxythetalat/environment_navxythetalat.h
+++ src/discrete_space_information/navxythetalat/environment_navxythetalat.h
@@ -168,6 +168,9 @@ typedef struct ENV_NAVXYTHETALAT_CONFIG
 
 class SBPL2DGridSearch;
 
+//3D (x,y,theta) planning using lattice-based graph problem. For general structure see comments on parent class DiscreteSpaceInformation
+//For info on lattice-based planning used here, you can check out the paper: 
+//Maxim Likhachev and Dave Ferguson, " Planning Long Dynamically-Feasible Maneuvers for Autonomous Vehicles", IJRR'09
 class EnvironmentNAVXYTHETALATTICE : public DiscreteSpaceInformation
 {
 
@@ -175,24 +178,57 @@ public:
 
 	EnvironmentNAVXYTHETALATTICE();
 
+	//initialization of environment from file. See .cfg files for examples
+	//it also takes the perimeter of the robot with respect to some reference point centered at x=0,y=0 and orientation = 0 (along x axis). 
+	//The perimeter is defined in meters as a sequence of vertices of a polygon defining the perimeter. If vector is of zero size, then
+	//robot is assumed to be point robot (you may want to inflate all obstacles by its actual radius)
+	//Motion primitives file defines the motion primitives available to the robot
 	bool InitializeEnv(const char* sEnvFile, const vector<sbpl_2Dpt_t>& perimeterptsV, const char* sMotPrimFile);	
+	//see comments on the same function in the parent class
 	bool InitializeEnv(const char* sEnvFile);
+	//way to set up various parameters. For a list of parameters, see the body of the function - it is pretty straightforward
 	virtual bool SetEnvParameter(const char* parameter, int value);
+	//returns the value of specific parameter - see function body for the list of parameters
 	virtual int GetEnvParameter(const char* parameter);
+	//see comments on the same function in the parent class
 	bool InitializeMDPCfg(MDPConfig *MDPCfg);
+	//see comments on the same function in the parent class
 	virtual int  GetFromToHeuristic(int FromStateID, int ToStateID) = 0;
+	//see comments on the same function in the parent class
 	virtual int  GetGoalHeuristic(int stateID) = 0;
+	//see comments on the same function in the parent class
 	virtual int  GetStartHeuristic(int stateID) = 0;
+	//see comments on the same function in the parent class
 	virtual void SetAllActionsandAllOutcomes(CMDPSTATE* state) = 0;
+	//see comments on the same function in the parent class
 	virtual void SetAllPreds(CMDPSTATE* state);
+	//see comments on the same function in the parent class
 	virtual void GetSuccs(int SourceStateID, vector<int>* SuccIDV, vector<int>* CostV);
+	//see comments on the same function in the parent class
 	virtual void GetPreds(int TargetStateID, vector<int>* PredIDV, vector<int>* CostV) = 0;
 
-	virtual void EnsureHeuristicsUpdated(bool bGoalHeuristics); //see comments in environment.h
+	//see comments on the same function in the parent class
+	virtual void EnsureHeuristicsUpdated(bool bGoalHeuristics); 
 
 
+	//see comments on the same function in the parent class
 	void PrintEnv_Config(FILE* fOut);
 
+	//initialize environment. Gridworld is defined as matrix A of size width by height. 
+	//So, internally, it is accessed as A[x][y] with x ranging from 0 to width-1 and and y from 0 to height-1
+	//Each element in A[x][y] is unsigned char. A[x][y] = 0 corresponds to fully traversable and cost is just Euclidean distance
+	//The cost of transition between two neighboring cells is EuclideanDistance*(max(A[sourcex][sourcey],A[targetx][targety])+1)
+	//If A[x][y] >= obsthresh, then in the above equation it is assumed to be infinite.
+	//The cost also incorporates the length of a motion primitive and its cost_multiplier (see getcost function)
+	//mapdata is a pointer to the values of A. If it is null, then A is initialized to all zeros. Mapping is: A[x][y] = mapdata[x+y*width]
+	//start/goal are given by startx, starty, starttheta, goalx,goaly, goaltheta in meters/radians. 
+	//If they are not known yet, just set them to 0. Later setgoal/setstart can be executed
+	//finally obsthresh defined obstacle threshold, as mentioned above
+	//goaltolerances are currently ignored
+	//for explanation of perimeter, see comments for InitializeEnv function that reads all from file
+	//cellsize is discretization in meters
+	//nominalvel_mpersecs is assumed velocity of vehicle while moving forward in m/sec
+	//timetoturn45degsinplace_secs is rotational velocity in secs/45 degrees turn
     bool InitializeEnv(int width, int height,
 		       /** if mapdata is NULL the grid is initialized to all freespace */
                        const unsigned char* mapdata,
@@ -202,29 +238,37 @@ public:
 					   const vector<sbpl_2Dpt_t> & perimeterptsV,
 					   double cellsize_m, double nominalvel_mpersecs, double timetoturn45degsinplace_secs, 
 					   unsigned char obsthresh, const char* sMotPrimFile);
+	//update the traversability of a cell<x,y>
     bool UpdateCost(int x, int y, unsigned char newcost);
+	//this function fill in Predecessor/Successor states of edges whose costs changed
+	//It takes in an array of cells whose traversability changed, and returns (in vector preds_of_changededgesIDV) 
+	//the IDs of all states that have outgoing edges that go through the changed cells
 	virtual void GetPredsofChangedEdges(vector<nav2dcell_t> const * changedcellsV, vector<int> *preds_of_changededgesIDV) = 0;
+	//same as GetPredsofChangedEdges, but returns successor states. Both functions need to be present for incremental search
 	virtual void GetSuccsofChangedEdges(vector<nav2dcell_t> const * changedcellsV, vector<int> *succs_of_changededgesIDV) = 0;
 
 
-
+	//returns true if cell is untraversable
 	bool IsObstacle(int x, int y);
+	//returns false if robot intersects obstacles or lies outside of the map. Note this is pretty expensive operation since it computes the footprint
+	//of the robot based on its x,y,theta
 	bool IsValidConfiguration(int X, int Y, int Theta);
 
+	//returns environment parameters. Useful for creating a copy environment
 	void GetEnvParms(int *size_x, int *size_y, double* startx, double* starty, double* starttheta, double* goalx, double* goaly, double* goaltheta,
 			double* cellsize_m, double* nominalvel_mpersecs, double* timetoturn45degsinplace_secs, unsigned char* obsthresh, vector<SBPL_xytheta_mprimitive>* motionprimitiveV);
-
+	//get internal configuration data structure
 	const EnvNAVXYTHETALATConfig_t* GetEnvNavConfig();
 
 
     virtual ~EnvironmentNAVXYTHETALATTICE();
-
+	//prints time statistics
     void PrintTimeStat(FILE* fOut);
-  
+	//returns the cost corresponding to the cell <x,y>
 	unsigned char GetMapCost(int x, int y);
 
-  
-  bool IsWithinMapCell(int X, int Y);
+	//returns true if cell is within map
+	bool IsWithinMapCell(int X, int Y);
   
   /** Transform a pose into discretized form. The angle 'pth' is
       considered to be valid if it lies between -2pi and 2pi (some
@@ -253,6 +297,7 @@ public:
   bool PoseDiscToCont(int ix, int iy, int ith,
 		      double &px, double &py, double &pth) const;
 
+  //prints environment variables for debugging
   virtual void PrintVars(){};
 
  protected:
@@ -333,30 +378,48 @@ class EnvironmentNAVXYTHETALAT : public EnvironmentNAVXYTHETALATTICE
 
   ~EnvironmentNAVXYTHETALAT();
 
+  //sets start in meters/radians
   int SetStart(double x, double y, double theta);
+  //sets goal in meters/radians
   int SetGoal(double x, double y, double theta);
+  //sets goal tolerance. (Note goal tolerance is ignored currently)
   void SetGoalTolerance(double tol_x, double tol_y, double tol_theta) { /**< not used yet */ }
-
+  //returns state coordinates of state with ID=stateID
   void GetCoordFromState(int stateID, int& x, int& y, int& theta) const;
+  //returns stateID for a state with coords x,y,theta
   int GetStateFromCoord(int x, int y, int theta);
 
+  //converts a path given by stateIDs into a sequence of coordinates. Note that since motion primitives are short actions represented as a sequence of points,
+  //the path returned by this function contains much more points than the number of points in the input path. The returned coordinates are in meters,meters,radians
   void ConvertStateIDPathintoXYThetaPath(vector<int>* stateIDPath, vector<EnvNAVXYTHETALAT3Dpt_t>* xythetaPath); 
+  //prints state info (coordinates) into file
   void PrintState(int stateID, bool bVerbose, FILE* fOut=NULL);
-
+  //returns all predecessors states and corresponding costs of actions
   virtual void GetPreds(int TargetStateID, vector<int>* PredIDV, vector<int>* CostV);
+  //returns all successors states, costs of corresponding actions and pointers to corresponding actions, each of which is a motion primitive
+  //if actionindV is NULL, then pointers to actions are not returned
   virtual void GetSuccs(int SourceStateID, vector<int>* SuccIDV, vector<int>* CostV, vector<EnvNAVXYTHETALATAction_t*>* actionindV=NULL);
 
+  //this function fill in Predecessor/Successor states of edges whose costs changed
+  //It takes in an array of cells whose traversability changed, and returns (in vector preds_of_changededgesIDV) 
+  //the IDs of all states that have outgoing edges that go through the changed cells
   void GetPredsofChangedEdges(vector<nav2dcell_t> const * changedcellsV, vector<int> *preds_of_changededgesIDV);
+  //same as GetPredsofChangedEdges, but returns successor states. Both functions need to be present for incremental search
   void GetSuccsofChangedEdges(vector<nav2dcell_t> const * changedcellsV, vector<int> *succs_of_changededgesIDV);
 
+  //see comments on the same function in the parent class
   virtual void SetAllActionsandAllOutcomes(CMDPSTATE* state);
 
+  //see comments on the same function in the parent class
   virtual int  GetFromToHeuristic(int FromStateID, int ToStateID);
+  //see comments on the same function in the parent class
   virtual int  GetGoalHeuristic(int stateID);
+  //see comments on the same function in the parent class
   virtual int  GetStartHeuristic(int stateID);
 
+  //see comments on the same function in the parent class
   virtual int	 SizeofCreatedEnv();
-
+  //see comments on the same function in the parent class
   virtual void PrintVars(){};
 
  protected:
