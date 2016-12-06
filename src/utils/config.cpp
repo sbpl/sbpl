251b04b2b4b601beea9d4e2cec482c02a7a4a525
Andrew Dornbush <andrew.dornbush@gmail.com>
2016-12-06 11:03:34 -0500

    fix missing return value from default file stream flush

--- src/utils/config.cpp
+++ src/utils/config.cpp
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2008, Maxim Likhachev
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of the Carnegie Mellon University nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -131,6 +131,6 @@ int SBPL_DEFAULT_DEBUG_OUTPUT_LOGGER(int level, const char* msg)
 
 int SBPL_DEFAULT_DEBUG_OUTPUT_FLUSH(FILE* f)
 {
-    fflush(f);
+    return fflush(f);
 }
 
